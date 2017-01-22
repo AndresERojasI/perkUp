f6b4e0a9c1c6703ff1b2fbf47992ccd4bacd7c8b
Andres Rojas <andres.rojas.isaza@gmail.com>
2017-01-22 16:31:36 -0500

    Added created_at updated_at columns to all tables

---
+++ PerkUp/perkup_database/versions/1f8298a04a0e_add_updated_created_at_columns.py
@@ -0,0 +1,29 @@
+"""Add updated/created at columns
+
+Revision ID: 1f8298a04a0e
+Revises: 7486d3048d29
+Create Date: 2017-01-22 15:39:58.500628
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '1f8298a04a0e'
+down_revision = '7486d3048d29'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.add_column('organization', sa.Column('created_at', sa.DateTime()))
+    op.add_column('organization', sa.Column('update_at', sa.DateTime()))
+    op.add_column('datasource', sa.Column('created_at', sa.DateTime()))
+    op.add_column('datasource', sa.Column('updated_at', sa.DateTime()))
+    op.add_column('user', sa.Column('created_at', sa.DateTime()))
+    op.add_column('user', sa.Column('updated_at', sa.DateTime()))
+
+
+def downgrade():
+    pass
